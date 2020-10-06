505cb83870b9db3879cba6faed504d78d5a9e13d
Steven Mayo <steven.mayo16@gmail.com>
2020-10-06 17:34:43 -0400

    Beenis

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from lex import l
 
-for token in l.lex("hel2LO <= 1.1 + 2 * lad \"Get utterly destroyed, scrub\""):
+file_path = "input.txt"
+
+file = open(file_path, 'r')
+
+for token in l.lex(file.read()):
     print(token)
+
+for token in l.lex("hel2LO <= 1.1 + 2 * lad \"Get utterly destroyed, scrub\""):
+    print(token.source_pos)
