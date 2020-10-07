0c04de0d9af572a8b51d0ae9d8a0d29c519e345c
Steven Mayo <steven.mayo16@gmail.com>
2020-10-06 22:44:48 -0400

    Made input.txt use a basic loop to prove they work

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ file = open(file_path, 'r')
 for token in l.lex(file.read()):
     print(token)
 
-for token in l.lex("hel2LO <= 1.1 + 2 * lad \"Get utterly destroyed, scrub\""):
-    print(token.source_pos)
+# for token in l.lex("hel2LO <= 1.1 + 2 * lad \"Get utterly destroyed, scrub\""):
+#     print(token)
+#     print(token.source_pos)
