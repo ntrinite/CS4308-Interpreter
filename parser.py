24273f5235f4d6b27ca9003179f1d0946f56972f
Steven Mayo <steven.mayo16@gmail.com>
2020-10-19 16:58:35 -0400

    Trying and failing to get started on doing it recursively

--- parser.py
+++ parser.py
@@ -6,5 +6,24 @@ file = open(file_path, 'r')
 
 status = []
 
-for token in l.lex(file.read()):
-    print(token.name)
+tokens = l.lex(file.read())
+currentTokenIndex = 0
+
+# def block(nextToken)
+#     print("<block> -> <statement>")
+#     nextToken = statement(nextToken)
+#
+#     if nextToken != None:
+#         print("<block> -> <statement> <block>")
+#         valid block(nextToken)
+#     return valid
+#
+# def statement(nextToken):
+#     return True
+#
+# def arithmetic_op(nextToken):
+#     name = tokens[currentTokenIndex].name
+#     if (name == 'PLUS' or name == 'MINUS' or name == 'DIVISION' or name == 'MULTIPLICATION'):
+#         nextToken
+#         return nextToken
+#     return nextToken
